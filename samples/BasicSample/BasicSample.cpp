646779d100648e81dd69e1b3302963b8a4a72776
Pedro Henrique <ph.geekstuff@gmail.com>
2016-05-11 19:35:00 +0100

    Adding BasicSampe project. It's an initial version of sample project to use wtl written in Visual C++ 2015.

---
+++ samples/BasicSample/BasicSample.cpp
@@ -0,0 +1,17 @@
+// BasicSample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <wtl.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "First wtl Example." << endl;
+	cout << "------------------" << endl;
+
+	return 0;
+}
+
